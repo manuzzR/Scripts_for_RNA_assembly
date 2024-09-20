a3848baf2cdcb3207298f5b0920cca13ed4f6c37
Manu <76021656+manuzzR@users.noreply.github.com>
2024-09-20 10:50:11 -0600

    Update infromation

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --mem=100G
 #SBATCH --time=12:00:00
-
+## this code we used for job submission in compute Canada server.
 module load StdEnv/2020
 module load fastqc/0.11.9
 
 fastqc -o fastqc_result -t 6 *fastq.gz
+#result will show the quality of our raw RNA-seq data.
 
